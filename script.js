9dad6f5144827cd9b840d8370b185b462357ed1a
KanimozhiSivakumar <118879210+KanimozhiSivakumar@users.noreply.github.com>
2023-07-14 13:57:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+// function wish(){
+//     var a = 10;
+//     for(var i=1;i<=a;i++)
+//     {
+//         console.log(a[i]);
+//     }
+// }
+
+// setTimeout(wish(),2000);
+
+// const display = () => {
+//     setTimeout(()=>{
+//         console.log("hello kanimozhi");
+//     },3000);
+// };
+
+// display();
+
+
+// const display1 = () => {
+//     setInterval(()=>{
+//         console.log("hello kanimozhi");
+//     },3000);
+// };
+
+// display1();
+
+const festivel = () => {
+    setTimeout(()=>{
+        setTimeout(()=>{
+            setTimeout(()=>{
+                setTimeout(()=>{
+                    setTimeout(()=>{
+                        setTimeout(()=>{
+                            setTimeout(()=>{
+                                setTimeout(()=>{
+                                    setTimeout(()=>{
+                                        setTimeout(()=>{
+                                            setTimeout(()=>{
+                                                const para = document.createElement("h1");
+                                                para.innerText="Happy Independent day";
+                                                document.body.appendChild(para);
+                                        
+                                            },1000);
+                                            const para = document.createElement("h1");
+                                            para.innerText="1";
+                                            document.body.appendChild(para);
+                                    
+                                        },1000);
+                                        const para = document.createElement("h1");
+                                        para.innerText="2";
+                                        document.body.appendChild(para);
+                                
+                                    },1000);
+                                    const para = document.createElement("h1");
+                                    para.innerText="3";
+                                    document.body.appendChild(para);
+                            
+                                },1000);
+                                const para = document.createElement("h1");
+                                para.innerText="4";
+                                document.body.appendChild(para);
+                        
+                            },1000);
+                            const para = document.createElement("h1");
+                            para.innerText="5";
+                            document.body.appendChild(para);
+                    
+                        },1000);
+                        const para = document.createElement("h1");
+                        para.innerText="6";
+                        document.body.appendChild(para);
+                
+                    },1000);
+                    const para = document.createElement("h1");
+                    para.innerText="7";
+                    document.body.appendChild(para);
+            
+                },1000);
+                const para = document.createElement("h1");
+                para.innerText="8";
+                document.body.appendChild(para);
+        
+            },1000);
+            const para = document.createElement("h1");
+            para.innerText="9";
+            document.body.appendChild(para);
+    
+        },1000);
+        const para = document.createElement("h1");
+        para.innerText="10";
+        document.body.appendChild(para);
+
+    },2000);
+};
+
+
+festivel();
+
+
+
